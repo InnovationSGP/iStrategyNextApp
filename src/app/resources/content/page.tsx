fe94f9ab498aef0219699673776f53b5028d89bc
brutussegun <brutussegun@gmail.com>
2023-03-13 19:21:54 -0400

    fixed blogcarousel

--- src/app/resources/content/page.tsx
+++ src/app/resources/content/page.tsx
@@ -2,10 +2,7 @@
 import { FunctionComponent, Suspense, useEffect, useState } from "react";
 import { useRouter, useSearchParams, usePathname } from "next/navigation";
 import Loading from "@/app/components/Loading";
-import {
-  useGetBlogs_Public,
-  useGetEachBlog_Public,
-} from "../../../pages/api/routes/blogRoute";
+import { useGetEachBlog_Public } from "../../../pages/api/routes/blogRoute";
 import ErrorComponent from "@/app/components/Error";
 import LockClosedIcon from "@heroicons/react/24/outline/LockClosedIcon";
 import ServerIcon from "@heroicons/react/24/outline/ServerIcon";
@@ -16,8 +13,8 @@ import { blogsRoute } from "../../../pages/api/routes/blogRoute";
 import { HeroCTA } from "../../components/HeroCTA";
 import Link from "next/link";
 import ImageFunction from "@/utils/ImageFunction";
-import { page_routes } from "@/lib/pageRoutes";
 import Image from "next/image";
+import { BlogCarousel } from "./BlogCarousel";
 
 interface BlogResourceProps {}
 
@@ -114,75 +111,6 @@ function Content(props: { blog: BlogObject }) {
           <BlogCarousel />
         </div>
       </div>
-    </div>
-  );
-}
-
-export const BlogCarousel = () => {
-  const { blogs, isLoading, isError } = useGetBlogs_Public();
-  return (
-    <section className="px-4 py-4 mx-auto bg-white ">
-      <h2 className="mb-2 text-3xl font-extrabold leading-tight text-primaryBlue py-4 items-center text-center">
-        Recent Resources{" "}
-      </h2>
-      <div className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 text-primaryBlue">
-        {blogs || blogs?.length > 0
-          ? blogs
-              .slice(0, 3)
-              .map((blog: BlogObject) => (
-                <BlogCarouselCard data={blog} key={blog._id} />
-              ))
-          : "No recent blogs"}
-      </div>
-      <div className="text-center pt-8 flex justify-end ">
-        <Link
-          className="text-xs md:text-lg font-bold shadow text-primaryBlue p-4 px-4 rounded-sm bg-gray-100 cursor-pointer hover:bg-primaryBlue hover:text-white hover:scale-105 transform ease-in-out duration-300 animate-bounce"
-          href={page_routes.resourceCenter}
-        >
-          Load More
-        </Link>
-      </div>
-    </section>
-  );
-};
-
-function BlogCarouselCard(props: { data: BlogObject }) {
-  return (
-    <div className="">
-      <Link href={`${page_routes.resourceCenter}/content?id=${props.data._id}`}>
-        <Image
-          src={props.data.img}
-          className="object-cover w-full h-56 mb-5 bg-center rounded"
-          alt={props.data.header}
-          loading="lazy"
-          width={400}
-          height={600}
-        />
-      </Link>
-      <h2 className="mb-2 text-lg font-semibold text-primaryBlue">
-        <Link
-          href={`${page_routes.resourceCenter}/content?id=${props.data._id}`}
-          className="text-primaryBlue hover:text-purple-700 capitalize"
-        >
-          {props.data.header}
-        </Link>
-      </h2>
-
-      <p className="mb-3 text-sm font-normal text-gray-500">
-        <Link
-          href={`${page_routes.resourceCenter}/content?id=${props.data._id}`}
-          className="font-medium text-primaryBlue hover:text-purple-700"
-        >
-          {props.data.author}
-        </Link>{" "}
-        -{" "}
-        {new Date(props.data.date).toLocaleDateString(undefined, {
-          weekday: "long",
-          year: "numeric",
-          month: "long",
-          day: "numeric",
-        })}
-      </p>
     </div>
   );
 }
