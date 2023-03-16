23e12b9c6eaa406c588f2dd3475eff262f3e9920
brutussegun <brutussegun@gmail.com>
2023-03-16 10:31:13 -0400

    changed env

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,7 +6,6 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { compare } from "bcryptjs";
 
 export const authOptions: NextAuthOptions = {
-  // site: process.env.NEXTAUTH_URL,
   secret: process.env.SECRET,
   session: {
     strategy: "jwt",
