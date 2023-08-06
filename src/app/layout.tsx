4fb4206c8a4c0b68b9252bd3e9e430a7c49f70e1
onemanarmy <brutussegun@gmail.com>
2023-08-06 17:20:42 -0500

    added page header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,37 +5,38 @@ import "./wordpress.css";
 import "react-quill/dist/quill.snow.css";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
-import { Toaster } from "react-hot-toast";
-import { SessionProvider } from "next-auth/react";
-import { Suspense } from "react";
+import {Toaster} from "react-hot-toast";
+import {SessionProvider} from "next-auth/react";
+import {Suspense} from "react";
 import Loading from "./components/Loading";
-import { ApolloProvider } from "@apollo/client";
-import { useApollo } from "../lib/apollo";
+import {ApolloProvider} from "@apollo/client";
+import {useApollo} from "@/lib/apollo";
+import Head from "@/app/head";
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }) {
-  const toastOptions = {
-    className:
-      "border-2 border-secondaryPurpleLight p-4 font-secondaryPurpleLight",
-  };
+    const toastOptions = {
+        className:
+            "border-2 border-secondaryPurpleLight p-4 font-secondaryPurpleLight",
+    };
 
-  const client = useApollo();
-  return (
-    <html lang="en">
-      <head />
-      <ApolloProvider client={client}>
-        <SessionProvider>
-          <body className=" overflow-x-hidden  ">
-            <Toaster toastOptions={toastOptions} />
-            <Navigation />
-            <Suspense fallback={<Loading />}>{children}</Suspense>
-            <Footer />
-          </body>
-        </SessionProvider>
-      </ApolloProvider>
-    </html>
-  );
+    const client = useApollo();
+    return (
+        <html lang="en">
+        <Head/>
+        <ApolloProvider client={client}>
+            <SessionProvider>
+                <body className=" overflow-x-hidden  ">
+                <Toaster toastOptions={toastOptions}/>
+                <Navigation/>
+                <Suspense fallback={<Loading/>}>{children}</Suspense>
+                <Footer/>
+                </body>
+            </SessionProvider>
+        </ApolloProvider>
+        </html>
+    );
 }
