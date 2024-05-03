29d45d2ca8bae1caeba5323c4e82082198fd2058
onemanarmy <brutussegun@gmail.com>
2024-05-03 18:01:57 -0500

    updated the error in nextconfig

--- next.config.js
+++ next.config.js
@@ -1,20 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-  },
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    domains: ["res.cloudinary.com", "www.wordpress.innovationsgp.com"],
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+    env: {
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    },
+
+    images: {
+        domains: ["res.cloudinary.com", "www.wordpress.innovationsgp.com"],
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
 };
 
 module.exports = nextConfig;
