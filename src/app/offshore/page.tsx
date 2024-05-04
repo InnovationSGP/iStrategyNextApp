333301c6533de96b73ff5dbe7dcf2ddc3cb0e784
onemanarmy <brutussegun@gmail.com>
2024-05-04 18:42:10 -0500

    auto signed by brutus segun

--- src/app/offshore/page.tsx
+++ src/app/offshore/page.tsx
@@ -4,13 +4,12 @@ import Feature from "../components/Features/FeatureSection";
 import {BlogCarousel} from "../resources/content/BlogCarousel";
 import {PrimaryCTA} from "../components/Hero/HeroCTA";
 import {Testimonial} from "../components/Testimonial/Testimonial";
-import OffshorePNG from "../../../public/assets/remoteWorker.png"
-import {OffshoreHero} from "../components/OffshoreHero";
+
 
 export default function Offshore() {
     return (
         <div>
-       
+
             <Feature/>
             <Testimonial/>
             <PrimaryCTA/>
