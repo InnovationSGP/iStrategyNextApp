99523becb6d8ce870fdedfcbe41a9b56eaf91734
onemanarmy <brutussegun@gmail.com>
2024-05-04 10:32:48 -0500

    auto

--- next.config.js
+++ next.config.js
@@ -2,19 +2,17 @@
 const nextConfig = {
     env: {
         NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET
     },
-    experimental: {
-        appDir: true,
-    },
+
     images: {
-        domains: ["res.cloudinary.com", "www.wordpress.innovationsgp.com"],
+        domains: ["res.cloudinary.com", "www.wordpress.innovationsgp.com"]
     },
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
         ignoreDuringBuilds: true,
-    },
+    }
 };
 
 module.exports = nextConfig;
